c2f3b697db3e6370e01f672378f17f9f99045661
A-nthony <68430761+A-nthony@users.noreply.github.com>
2022-12-13 23:35:50 -0500

    Update database.js
    Actualizar mensaje de conexion satisfactoria

--- src/database.js
+++ src/database.js
@@ -5,5 +5,5 @@ mongoose.connect( process.env.MONGODB_URI,
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(db => console.log('Db connected! c:'))
+  .then(db => console.log('Db connected 🎉! c:'))
   .catch(err => console.log(err));
